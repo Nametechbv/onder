f04b9d00775a10d3a2269198a6c49cfe7e17cbe9
RubixB2 <alperc.business@gmail.com>
2026-06-29 16:15:48 +0200

    aarubix

---
+++ aarubixb2_custom_fields/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Partner Form Custom Fields',
+    'version': '1.0',
+    'summary': 'Adds a readonly related field to partner showing pricelist ID',
+    'author': 'rubixb2',
+    'depends': ['base', 'product', 'sale', 'stock', 'account'],
+    #'data': [
+     #   'views/all_field_views.xml'
+    #], 
+    'installable': True,
+    'application': False,
+}
